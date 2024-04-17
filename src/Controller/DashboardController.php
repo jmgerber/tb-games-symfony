4dee008cce2fe759d81597461ae4a7c8476a600e
jmgerber <jmgerber57@gmail.com>
2024-04-17 17:36:00 +0200

    setting up role hierarchy and granted roles

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app.dashboard')]
+    #[IsGranted('ROLE_USER')]
     public function index(): Response
     {
         return $this->render('dashboard/index.html.twig');
