bc497a4ba70ad2a4ac688d9e26ec0fe48f00a15c
jmgerber <jmgerber57@gmail.com>
2024-06-13 11:33:48 +0200

    fix bug with double api calls

--- assets/controllers/dashboard_controller.js
+++ assets/controllers/dashboard_controller.js
@@ -5,46 +5,43 @@ import gameDifficultyShow from './difficulty-display_controller.js';
  * This Stimulus controller manages a game card container, fetching game data and displaying modal details on card click.
  */
 export default class extends Controller {
-    
+
     /**
-   * Fetches game data from the API and initializes the controller state.
-   * 
-   * @async
-   */
+     * Fetches game data from the API on controller initialization.
+     * Handles potential errors and sets the `games` property.
+     * 
+     * @returns {Promise | null} Promise resolving to fetched game data or null on error.
+     */
     async connect() {
+        this.games = await this.fetchGameData()
+
+        if (!this.games) return
+
+        this.initializeModalElements()
+        this.setupGameCards()
+    }
+
+    /**
+     * Fetches game data from the provided API endpoint.
+     * 
+     * @returns {Promise | null} Promise resolving to fetched game data or null on error.
+     */
+    async fetchGameData() {
         try {
             const response = await fetch('http://localhost:8000/api/games')
-            this.games = await response.json()
+            this.gamesData = await response.json()
+            return this.gamesData
         } catch (error) {
             console.error('Error fetching game data:', error)
+            return null
         }
-
-        this.modal = document.getElementById('modal')
-        this.initializeModalElements()
-
-        
-        const cards = document.querySelectorAll('.game-card')
-        cards.forEach(card => {
-            // Diplays the difficulty of each game with stars
-            const difficultyDisplay = card.querySelector('.game-difficulty')
-            const difficultyLevel = difficultyDisplay.dataset['difficulty']
-            if (difficultyDisplay.innerHTML === "") {
-                gameDifficultyShow(difficultyLevel).forEach(element => {
-                    difficultyDisplay.appendChild(element)
-                })
-            }   
-
-            // Adds a click event to each game card to display the modal
-            card.addEventListener('click', this.showModalDetails.bind(this))
-        })
-
-        this.closeButton.addEventListener('click', this.hideModal.bind(this))
     }
 
     /**
      * Initializes references to modal elements within the DOM.
      */
     initializeModalElements() {
+        this.modal = document.getElementById('modal')
         this.modalTitle = this.modal.querySelector('.modal-title')
         this.modalPicture = this.modal.querySelector('.modal-picture')
         this.modalDescription = this.modal.querySelector('.modal-description')
@@ -52,6 +49,27 @@ export default class extends Controller {
         this.modalTime = this.modal.querySelector('.time-text')
         this.closeButton = this.modal.querySelector('.close-modal-btn')
         this.startButton = this.modal.querySelector('.start-button')
+    }
+
+    /**
+     * Sets up click events for game cards and the close button.
+     * Loops through all game cards, displays difficulty and adds click event.
+     */
+    setupGameCards() {
+        const cards = document.querySelectorAll('.game-card')
+        cards.forEach(card => {
+            // Diplays the difficulty of each game with stars
+            const difficultyDisplay = card.querySelector('.game-difficulty')
+            const difficultyLevel = difficultyDisplay.dataset['difficulty']
+            gameDifficultyShow(difficultyLevel).forEach(element => {
+                difficultyDisplay.appendChild(element)
+            })
+  
+            // Adds a click event to each game card to display the modal
+            card.addEventListener('click', this.showModalDetails.bind(this))
+        })
+
+        this.closeButton.addEventListener('click', this.hideModal.bind(this))
     }
 
     /**
@@ -80,7 +98,11 @@ export default class extends Controller {
         })
 
         this.modalTime.textContent = `${cardData.time} min`
-        this.startButton.setAttribute('href', '../game/' + cardData.id)
+        this.startButton.addEventListener('click', (event) => {
+            event.preventDefault()
+            window.location.href = `../game/${cardData.id}`
+            this.hideModal(event)
+        })
         this.modal.style.display = 'flex'
     }
     
